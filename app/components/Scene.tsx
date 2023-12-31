20476029c44e26798b2389b26bd5175f74a16b37
Adam Hitzger <99116869+adamhitzger@users.noreply.github.com>
2023-12-31 11:03:33 +0000

    commit

--- app/components/Scene.tsx
+++ app/components/Scene.tsx
@@ -2,12 +2,7 @@
 import React, { useMemo } from "react";
 
 import { Canvas } from "@react-three/fiber";
-import {
-  Float,
-  OrbitControls,
-  Environment,
-  PerspectiveCamera,
-} from "@react-three/drei";
+import { Float, OrbitControls, Environment } from "@react-three/drei";
 import { RootContainer } from "@coconut-xr/koestlich";
 import colors from "../theme/colors";
 import { Glass } from "@coconut-xr/apfel-kruemel";
@@ -22,7 +17,7 @@ export default function Scene({ children }: SceneProps) {
   const mesh = useMemo(() => new RectMesh(), []);
   return (
     <div className="h-screen">
-      <Canvas camera={{ position: [0, 5, 0] }}>
+      <Canvas camera={{ position: [0, 10, 4] }}>
         <Environment
           files={"/postdamer.hdr"}
           ground={{ height: 30, radius: 100, scale: 100 }}
